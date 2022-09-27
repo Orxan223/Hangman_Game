3fcf22b52b74285d0df8e6a917d7d988a7260b44
Orxan223 <or.xan.2018@bk.ru>
2022-09-28 02:34:22 +0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var sozler = ["alma", "armud", "banan", "nar"]
 
 var sans = document.querySelector('#sans')
-var u = document.querySelector('#udus')
+var q = document.querySelector('#qalibiyyet')
 var m = document.querySelector('#meglubiyyet')
 var i = document.querySelector('#isdifadeci')
 var d = document.querySelector('#duzgun')
@@ -13,26 +13,44 @@ var sozIndex = Math.floor(Math.random() * sozler.length);
 var soz = sozler[sozIndex]
 console.log(soz)
 var alt = [...soz].fill("_")
-console.log(d)
+console.log(alt)
 var texminler = []
 var limit = 9
-
+var qalib = 0
+var meglubiyyet = 0
 window.onkeyup = function (e) {
     for (let i = 0; i < soz.length; i++) {
-        if (soz[i] == e.key) {
-            alt[i] = e.key
-            d.innerHTML = alt
-        } 
-    }
-    texminler.push(e.key)
-    t.innerHTML = texminler
-    for (let i = 0; i < texminler.length; i++) {
-        
        
-        if (limit === 0) {
-           b.innerHTML = 'Oyun bitdi'
+        if (soz[i] == e.key) {
+            
+            d.innerHTML = alt
+            alt[i] = e.key
+            
+
+        } else if (alt.join('') == soz) {
+            qalib++
+            q.innerHTML = "Qalib oldunuz"
         }
+
+    }
+    
+texminler.push(e.key)
+t.innerHTML = texminler
+
+
+    for (let i = 0; i < texminler.length; i++) {
         sans.innerHTML = limit
+        if (limit === 0) {
+            qalib++
+            sans.innerHTML = alert('Oyun bitdi')
+
+            return
+
+
+        } else {
+            sans.innerHTML = limit
+        }
+
     }
     limit--
 
