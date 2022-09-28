3d62a6624a2f2dc42c89decf6da2291f3b48145f
Orxan223 <or.xan.2018@bk.ru>
2022-09-28 16:29:54 +0300

    finish

--- index.js
+++ index.js
@@ -7,47 +7,48 @@ var d = document.querySelector('#duzgun')
 var t = document.querySelector('#texmin')
 var b = document.querySelector('#bitdi')
 
-
 var sozIndex = Math.floor(Math.random() * sozler.length);
 var soz = sozler[sozIndex]
 console.log(soz)
+
 var alt = [...soz].fill("_")
 console.log(alt)
+d.innerHTML = alt
+
 var texminler = []
 var limit = 9
 var qalib = 0
-var meglubiyyet = 0
+
+
 window.onkeyup = function (e) {
     for (let i = 0; i < soz.length; i++) {
-       
-        if (soz[i] == e.key) {
-            
-            d.innerHTML = alt
+        if (soz[i] === e.key) {
             alt[i] = e.key
-            
-
-        } else if (alt.join('') == soz) {
-            qalib++
-            q.innerHTML = "Qalib oldunuz"
+            d.innerHTML = alt
+            if (alt.join('').includes('_') == false) {
+                console.log(alt)
+                console.log(alt.join('').includes('_') == false)
+                // alt xet qalmadisa qalib oluruq
+                qalib++
+                q.innerHTML = qalib
+                alert("Siz qalib oldunuz")
+            }
         }
-
     }
     
-texminler.push(e.key)
-t.innerHTML = texminler
 
-
+    texminler.push(e.key)
+    t.innerHTML = texminler
+    
+   
     for (let i = 0; i < texminler.length; i++) {
         sans.innerHTML = limit
         if (limit === 0) {
-            qalib++
             sans.innerHTML = alert('Oyun bitdi')
 
             return
 
 
-        } else {
-            sans.innerHTML = limit
         }
 
     }
